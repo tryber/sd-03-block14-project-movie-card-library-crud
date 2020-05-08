be11280c8cb2c998df604317ec38af5fb8f9151d
Hfreitas <hfreitas86@gmail.com>
2020-05-08 17:46:06 -0300

    Implementação do componente MovieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 class MovieCard extends React.Component {
   render() {
+    const { title, id } = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <h5>{title}</h5>
+        <Link to={`movies/:${id}`}>VER DETALHES</Link>
       </div>
     );
   }
