2fb62e4fdae6506603f080c4ddc0d6880afac0ee
Hfreitas <hfreitas86@gmail.com>
2020-05-08 18:07:41 -0300

    Inclusa validação de tipos apontada pelo code climate no componente MovieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
@@ -12,5 +13,11 @@ class MovieCard extends React.Component {
     );
   }
 }
+
+MovieCard.propTypes = {
+  id: PropTypes.number,
+  movie: PropTypes.object.isRequired,
+  title: PropTypes.string,
+};
 
 export default MovieCard;
