aa40a05f749a2eed6508db371ec02e2f1270ebb2
Hfreitas <hfreitas86@gmail.com>
2020-05-08 18:27:49 -0300

    Corrigido apontamento do Code Climate

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import MovieList from './pages/MovieList';
 
 function App() {
