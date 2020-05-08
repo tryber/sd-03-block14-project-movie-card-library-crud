2d2f377ae743ddd37996a37ca451c987c1051797
Hfreitas <hfreitas86@gmail.com>
2020-05-08 17:17:23 -0300

    Implementação do componente MovieList corrigida

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -13,7 +13,7 @@ class MovieList extends Component {
   }
 
   componentDidMount() {
-    // movieAPI.getMovies().then((movies) => this.setState({ movies }));
+    movieAPI.getMovies().then((movies) => this.setState({ movies }));
   }
 
   render() {
