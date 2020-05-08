eec85f275a31f43c2dba612208c33861751f8f6c
Marco Barbosa <marco.meireles.b@gmail.com>
2020-05-08 18:12:41 -0300

    Renderizando a lista de filmes

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,13 +1,27 @@
 import React from 'react';
-// import { getMovies } from '../services/movieAPI';
+import { getMovies } from '../services/movieAPI';
 import { Loading, MovieCard } from './index';
 
 class MovieList extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      movies: [],
+    };
+  }
+
+  componentDidMount() {
+    getMovies()
+    .then(resolve => this.setState(()=>(
+      { movies:[...JSON.parse(localStorage.getItem('movies'))] }
+    )));
+  }
+
   render() {
     return (
       <div>
         <Loading />
-        <MovieCard />
+        { this.state.movies.map((m) => <MovieCard key={m.title} movie={m} />) }
       </div>
     );
   }
