2c5e64698c0dcbbf944e6bb0665c56551e64a544
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-05-08 19:18:20 -0300

    Adiciona storyline ao MovieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,10 +3,11 @@ import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
-    const { title, id } = this.props.movie;
+    const { title, id, storyline } = this.props.movie;
     return (
       <div data-testid="movie-card">
         <h4>{title}</h4>
+        <p>{storyline}</p>
         <Link to={`/movies/${id}`}>VER DETALHES</Link>
       </div>
     );
