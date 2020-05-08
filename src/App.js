fa4e57fd425d72b5636b786365bbb9e1e1e49b63
Lucas-Werner <lucas.1werner@gmail.com>
2020-05-08 15:28:16 -0300

    iniciando projeto

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
+import './App.css';
 
 function App() {
   return (
-    <div>Movie Card Library Crud</div>
+    <BrowserRouter>
+      <main>
+        <Switch>
+          <Route exact path="/" component={MovieList} />
+          <Route exact path="/movies/new" component={NewMovie} />
+          <Route exact path="/movies/:id/edit" component={EditMovie} />
+          <Route exact path="/movies/:id" component={MovieDetails} />
+          <Route component={NotFound} />
+        </Switch>
+      </main>
+    </BrowserRouter>
   );
 }
 
