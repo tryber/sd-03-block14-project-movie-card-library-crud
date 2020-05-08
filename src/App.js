3df47f323b4919299aa6e44b78fd7773b2cea234
William Igor <williamigorsilva@gmail.com>
2020-05-08 15:28:39 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { BrowserRouter as Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>Movie Card Library Crud</div>
+    <Route>
+      <div>Movie Card Library Crud</div>
+    </Route>
   );
 }
 
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
+import Loading from '../components/Loading';
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   render() {
     const { movies } = this.state;
-
+    
+    getMovies() {
+      fetch()
+    }
+    
     // Render Loading here if the request is still happening
-
+    <Loading/>
+    
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
