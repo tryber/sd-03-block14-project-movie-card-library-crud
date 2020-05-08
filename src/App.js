6bbc1f3537d69300abf8a13a4ceccd3e642142d8
Hfreitas <hfreitas86@gmail.com>
2020-05-08 16:54:20 -0300

    Implementação do requisito 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import MovieList from './pages/MovieList';
 
 function App() {
   return (
     <BrowserRouter>
       <div>Movie Card Library Crud</div>
+      <div>
+        <MovieList />
+      </div>
     </BrowserRouter>
   );
 }
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,30 +1,35 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
+import Loading from '../components/Loading';
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
-      movies: [...movies],
-    }
+      movies: [],
+    };
   }
 
-componentDidMount () {
-  movieAPI.getMovies()
-  .then(response => response.json())
-}
+  componentDidMount() {
+    // movieAPI.getMovies().then((movies) => this.setState({ movies }));
+  }
 
   render() {
     const { movies } = this.state;
 
     // Render Loading here if the request is still happening
 
-    return (
+    return movies === [] ? (
+      <div>
+        <Loading />
+      </div>
+    ) : (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+        {movies.map((movie) => (
+          <MovieCard key={movie.title} movie={movie} />
+        ))}
       </div>
     );
   }
