1face6a3703a7ec10b996e2e9f8b4bd9e3d227c5
Gustaff <gustavohmfigueiredo@gmail.com>
2020-05-08 15:39:04 -0300

    texto de loading corrigido

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
 
 import * as movieAPI from '../services/movieAPI';
 
@@ -20,7 +21,7 @@ class MovieList extends Component {
   render() {
     const { movies } = this.state;
 
-    // Render Loading here if the request is still happening
+    if (movies) return <Loading />
 
     return (
       <div data-testid="movie-list">
