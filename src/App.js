612f8c910170695bc17b2012bc506b0665cba2a8
Rodrigo Militão <rodrigo_militao@hotmail.com.br>
2020-05-08 16:59:35 -0300

    movie list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import MovieList from './pages/MovieList';
 
 function App() {
   return (
     <Router>
       <div>Movie Card Library CRUD</div>
+      
+    <Route exact path="/" component={MovieList} />
+
     </Router>
   );
 }
