3519bfee6d11852527f42a059b1367b94d5f0e89
Henrique Rezende <henriquerezendec@gmail.com>
2020-05-08 21:20:33 -0300

    .

--- src/App.js
+++ src/App.js
@@ -11,11 +11,10 @@ function App() {
   return (
     <Router>
       <Switch>
-      <Route exact path="/" component={MovieList} />
         <Route exact path="/movies/new" component={NewMovie} />
-
         <Route exact path="/movies/:id" component={MovieDetails} />
         <Route exact path="/movies/:id/edit" component={EditMovie} />
+        <Route exact path="/" component={MovieList} />
         <Route component={NotFound} />
       </Switch>
     </Router>
