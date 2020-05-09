6614a8faae8eddcbf035c9d085aaaa3008878d32
mirellaalves <mirella.alves@gmail.com>
2020-05-09 16:05:02 -0300

    Including routes - requirement 1.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
 
 function App() {
   return (
     <Router>
-      <div>Movie Card Library Crud</div>
-      <div>
-        <MovieList />
-      </div>
+      <Switch>
+        <Route exact path="/" component={MovieList} />
+        <Route path="/movies/:id" component={MovieDetails}/>
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id/edit" component={EditMovie} />
+      </Switch>
     </Router>
   );
 }
