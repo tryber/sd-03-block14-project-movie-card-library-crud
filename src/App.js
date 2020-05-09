a7a9571f8bfef938dabc0a51449a21834eeab488
mirellaalves <mirella.alves@gmail.com>
2020-05-09 16:07:30 -0300

    Code climate issues.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <Router>
       <Switch>
         <Route exact path="/" component={MovieList} />
-        <Route path="/movies/:id" component={MovieDetails}/>
+        <Route path="/movies/:id" component={MovieDetails} />
         <Route path="/movies/new" component={NewMovie} />
         <Route path="/movies/:id/edit" component={EditMovie} />
       </Switch>
