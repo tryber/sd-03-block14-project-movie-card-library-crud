c24d6f462cd9deff745e0b91181e3b3be95af554
Clayton Pereira <cpereiramt@gmail.com>
2020-05-08 20:40:34 -0400

    Salvando as alteraçoes no projeto

--- src/App.js
+++ src/App.js
@@ -5,12 +5,20 @@ import {
     Switch,
 } from 'react-router-dom';
 import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/" component={MovieList} />
+        <Route path="/movies/:id" component={MovieDetails} />
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route component={NotFound} />
       </Switch>
     </Router>
   );
