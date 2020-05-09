2749eac7f036f9ce586119d018d4d926c81f28dc
mirellaalves <mirella.alves@gmail.com>
2020-05-09 19:31:43 -0300

    Requiring movie title.

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -25,7 +25,7 @@ class MovieCard extends React.Component {
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
-    title: PropTypes.string,
+    title: PropTypes.string.isRequired,
     subtitle: PropTypes.string,
     storyline: PropTypes.string,
     rating: PropTypes.number,
