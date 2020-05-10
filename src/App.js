50d66a0e82bf1bca306d4b762c9b01ad8d9486b9
luldelrey <rpgstrikered@gmail.com>
2020-05-10 19:11:29 -0300

    Routes and loading page done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import * as index from './pages/index';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div>Movie Card Library Crud</div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={index.MovieList} />
+          <Route path="/movies/:id" component={index.MovieDetails} />
+          <Route path="/movies/new" component={index.NewMovie} />
+          <Route path="/movies/:id/edit" component={index.EditMovie} />
+          <Route component={index.NotFound} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
