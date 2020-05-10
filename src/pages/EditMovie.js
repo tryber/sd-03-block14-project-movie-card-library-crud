54eead7b41ff79a69f2dd792db589d6af22624d8
Hfreitas <hfreitas86@gmail.com>
2020-05-10 14:49:03 -0300

    implementando requisito 5

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,26 +1,49 @@
 import React, { Component } from 'react';
-
-import { MovieForm } from '../components';
+import { Redirect } from 'react-router-dom';
+import { MovieForm, Loading } from '../components';
 import * as movieAPI from '../services/movieAPI';
+import PropTypes from 'prop-types';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      status: 'loading',
+      shouldRedirect: false,
+      movie: undefined,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  componentDidMount() {
+    movieAPI.getMovie(this.props.match.params.id).then((movie) =>
+      this.setState({
+        status: '',
+        shouldRedirect: '',
+        movie,
+      }),
+    );
+  }
+
+  componentDidUpdate() {
+    return <Redirect to="/" />;
+  }
+
   handleSubmit(updatedMovie) {
+    movieAPI.updateMovie(updatedMovie);
+    this.setState({ status: '', shouldRedirect: true, movie: updatedMovie });
   }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
       // Redirect
+      <Redirect to="/" />;
     }
 
     if (status === 'loading') {
       // render Loading
+      <Loading />;
     }
 
     return (
@@ -30,5 +53,11 @@ class EditMovie extends Component {
     );
   }
 }
+
+EditMovie.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({ id: PropTypes.number }),
+  }).isRequired,
+};
 
 export default EditMovie;
