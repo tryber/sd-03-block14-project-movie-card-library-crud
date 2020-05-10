351725953d8a89053676e003234fa02e1a5b2679
Clayton Pereira <cpereiramt@gmail.com>
2020-05-10 19:33:41 -0400

    Salvando as alteraçoes no projeto

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -2,25 +2,45 @@ import React, { Component } from 'react';
 
 import { MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
+import Loading from '../components/Loading';
+import { Redirect } from 'react-router-dom';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      status: 'loading',
+      movie: '',
+      shouldRedirect: false,
+    }
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  componentDidMount() {
+    movieAPI.getMovie(this.props.match.params.id)
+      .then((data) => {
+        this.setState({
+          movie: data,
+          status: 'false'
+        })
+      });
+  }
+
   handleSubmit(updatedMovie) {
+    movieAPI.updateMovie(this.props.match.params.id)
+    this.setState({ shouldRedirect: true })
   }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
+      return <Redirect to="/" />
       // Redirect
     }
 
     if (status === 'loading') {
-      // render Loading
+      // render Loading      
+      return <Loading />
     }
 
     return (
