549229c2b797b8a502847025d5c723d56c96b854
luldelrey <rpgstrikered@gmail.com>
2020-05-10 19:12:54 -0300

    movie details done

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,22 +1,51 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
+
 class MovieDetails extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hasMovies: false,
+      movie: {},
+    };
+  }
+
+  changeState(flag, obj) {
+    this.setState((state) => {
+      return {
+        ...state,
+        hasMovies: flag,
+        movie: obj,
+      }
+    });
+  }
+
+  async componentDidMount() {
+    const movie = await movieAPI.getMovie(this.props.match.params.id);
+    this.changeState(true, movie);
+  }
+
   render() {
     // Change the condition to check the state
-    if (true) return <Loading />;
+    if (!this.state.hasMovies) return <Loading />;
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = movie;
+    const { id, title, storyline, imagePath, genre, rating, subtitle } = this.state.movie;
 
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={`../${imagePath}`} />
+        <h4>{`Title: ${title}`}</h4>
         <p>{`Subtitle: ${subtitle}`}</p>
         <p>{`Storyline: ${storyline}`}</p>
         <p>{`Genre: ${genre}`}</p>
         <p>{`Rating: ${rating}`}</p>
+        <Link to={`/movies/${id}/edit`}>EDITAR</Link>
+        <br />
+        <Link to={'/'}>VOLTAR</Link>
       </div>
     );
   }
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -2,12 +2,38 @@ import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
 
 import * as movieAPI from '../services/movieAPI';
+import { Loading } from '../components';
 
 class MovieList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hasMovies: false,
+      movies: {},
+    };
+  }
+
+  changeState(flag, obj) {
+    this.setState((state) => {
+      return {
+        ...state,
+        hasMovies: flag,
+        movies: obj,
+      }
+    });
+  }
+
+  async componentDidMount() {
+    const movies = await movieAPI.getMovies();
+    this.changeState(true, movies);
+  }
+
   render() {
     const { movies } = this.state;
 
     // Render Loading here if the request is still happening
+    if (!this.state.hasMovies) return <Loading />;
 
     return (
       <div data-testid="movie-list">
