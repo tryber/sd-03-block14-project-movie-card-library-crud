a4f23f4e622d583a28f2913cd3e4bc5156085abc
mirellaalves <mirella.alves@gmail.com>
2020-05-10 14:32:19 -0300

    Working on EditMovie.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
 // import NewMovie from './pages/NewMovie';
-// import EditMovie from './pages/EditMovie';
+import EditMovie from './pages/EditMovie';
 
 function App() {
   return (
@@ -16,8 +16,8 @@ function App() {
         <Switch>
           <Route exact path="/" component={MovieList} />
           <Route path="/movies/:id" component={MovieDetails} />
-          {/* <Route path="/movies/new" component={NewMovie} />
-          <Route path="/movies/:id/edit" component={EditMovie} /> */}
+          {/* <Route path="/movies/new" component={NewMovie} /> */}
+          <Route path="/movies/:id/edit" component={EditMovie} />
         </Switch>
       </Router>
     </div>
--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,26 +1,39 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 
 import { MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
+import Loading from '../components/Loading';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      status: 'loading',
+      shouldRedirect: false,
+      movie: undefined,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  componentDidMount() {
+    movieAPI.getMovie(this.props.match.params.id)
+    .then((response) => this.setState({ movie: response, status: '' }));
+  }
+
   handleSubmit(updatedMovie) {
+    movieAPI.updateMovie(updatedMovie);
+    this.setState({ movie: updatedMovie, shouldRedirect: true });
   }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
-      // Redirect
+      return <Redirect to="/" />;
     }
 
     if (status === 'loading') {
-      // render Loading
+      return <Loading />
     }
 
     return (
