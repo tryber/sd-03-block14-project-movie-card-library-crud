f8860c65c6e3b5ccc346208f244c0c5469f88bba
DevT <aguiarthiag@gmail.com>
2020-05-10 18:40:28 -0300

    Corrigindo router

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Redirect, Router } from 'react-router-dom';
+import { Redirect, BrowserRouter} from 'react-router-dom';
 import MovieForm from '../components/MovieForm';
 import * as movieAPI from '../services/movieAPI';
 
@@ -18,7 +18,7 @@ class NewMovie extends Component {
   }
 
   render() {
-    if (this.state.redirect) return <Router><Redirect to="/" /></Router>;
+    if (this.state.redirect) return <BrowserRouter><Redirect to="/" /></BrowserRouter>;
     return (
       <div data-testid="new-movie">
         <MovieForm onSubmit={this.handleSubmit} />
