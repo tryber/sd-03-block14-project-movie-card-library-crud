e387cf20bbe9f761be5e7d70b2af22d9d8253a69
Fernando Luckesi <fernando.luckesi@94gmail.com>
2020-05-11 13:49:57 -0300

    Requisito 6 refeito

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import MovieForm from '../components/MovieForm';
 import * as movieAPI from '../services/movieAPI';
 
@@ -9,19 +10,24 @@ class NewMovie extends Component {
     super(props);
     this.state = {
       create: false,
+      route: '',
     };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(newMovie) {
+    const { history } = this.props;
     movieAPI.createMovie(newMovie)
-    .then(this.setState({ create: true }));
+    .then(this.setState({
+      create: true,
+      route: history.push('/'),
+    }));
   }
 
   render() {
-    const { create } = this.state;
+    const { create, route } = this.state;
 
-    if (create) return <Redirect to="/" />;
+    if (create) return <Redirect to={route} />;
 
     return (
       <div data-testid="new-movie">
@@ -30,5 +36,11 @@ class NewMovie extends Component {
     );
   }
 }
+
+NewMovie.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default NewMovie;
