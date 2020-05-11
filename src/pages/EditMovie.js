e364733dac5dd57708e422a2b75c822d9d62656a
mirellaalves <mirella.alves@gmail.com>
2020-05-11 14:24:55 -0300

    Code climate issues.

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -29,7 +29,7 @@ class EditMovie extends Component {
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
-    
+
     if (shouldRedirect) {
       return <Redirect to="/" />;
     }
