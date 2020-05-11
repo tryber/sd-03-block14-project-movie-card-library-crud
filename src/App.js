2642c0629f5e1cf32ab79cf5df504473835a10d5
luldelrey <rpgstrikered@gmail.com>
2020-05-11 12:38:28 -0300

    refactoring first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import M from 'materialize-css';
-import 'materialize-css/dist/css/materialize.min.css';
-import * as index from './pages/index';
+import { EditMovie, MovieDetails, MovieList, NewMovie, NotFound } from './pages/index';
 
 
 class App extends React.Component {
-
-  componentDidMount() {
-    M.AutoInit();
-  }
-
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={index.MovieList} />
-          <Route path="/movies/new" component={index.NewMovie} />
-          <Route exact path="/movies/:id" component={index.MovieDetails} />
-          <Route path="/movies/:id/edit" component={index.EditMovie} />
-          <Route component={index.NotFound} />
+          <Route exact path="/" component={MovieList} />
+          <Route path="/movies/new" component={NewMovie} />
+          <Route exact path="/movies/:id" component={MovieDetails} />
+          <Route path="/movies/:id/edit" component={EditMovie} />
+          <Route component={NotFound} />
         </Switch>
       </Router>
     );
