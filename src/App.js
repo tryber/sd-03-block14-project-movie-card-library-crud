4558204d7ecdef52be3a381a7391137e628808af
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-05-11 01:43:18 -0300

    cote

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch  } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
-import { NotFound, NewMovie, EditMovie, MovieDetails, MovieList} from './pages';
+import { NotFound, NewMovie, EditMovie, MovieDetails, MovieList } from './pages';
 
 function App() {
   return (
@@ -9,10 +9,10 @@ function App() {
       <BrowserRouter>
         <div>Movie Card Library CRUD</div>
         <Switch>
-          <Route exact path='/' component={MovieList} />
-          <Route exacy path='/movies/new'component={NewMovie} />
-          <Route exact path='/movies/:id' component={MovieDetails} />
-          <Route exact path='/movies/:id/edit' component={EditMovie} />
+          <Route exact path="/" component={MovieList} />
+          <Route exacy path="/movies/new"component={NewMovie} />
+          <Route exact path="/movies/:id" component={MovieDetails} />
+          <Route exact path="/movies/:id/edit" component={EditMovie} />
           <Route component={NotFound} />
         </Switch>
       </BrowserRouter>
