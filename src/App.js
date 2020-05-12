3d610de151d84ac33d294f91f8391c7f48f390ed
mirellaalves <mirella.alves@gmail.com>
2020-05-12 00:59:50 -0300

    Working on 404-error - page not found.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
 import NewMovie from './pages/NewMovie';
 import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route path="/movies/new" component={NewMovie} />
           <Route path="/movies/:id/edit" component={EditMovie} />
           <Route path="/movies/:id" component={MovieDetails} />
+          <Route component={NotFound} />
         </Switch>
       </Router>
     </div>
