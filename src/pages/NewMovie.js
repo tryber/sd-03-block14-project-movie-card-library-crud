db2c35f64d24dcfb7a47eda8ec11a60c61ed97eb
Fernando Luckesi <fernando.luckesi@94gmail.com>
2020-05-12 13:27:52 -0300

    Projeto finalizado + Bonus

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import MovieForm from '../components/MovieForm';
-import * as movieAPI from '../services/movieAPI'
+import * as movieAPI from '../services/movieAPI';
 
 
 class NewMovie extends Component {
