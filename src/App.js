9df2d4cf688d22fa1ca12d0e00d7ef8f2ac53d31
Marco Barbosa <marco.meireles.b@gmail.com>
2020-05-12 10:42:06 -0300

    codeclimate rev2

--- src/App.js
+++ src/App.js
@@ -7,17 +7,18 @@ import MovieList from './components/MovieList';
 import NewMovie from './components/NewMovie';
 import MovieDetails from './components/MovieDetails';
 import EditMovie from './components/EditMovie';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
     <Router>
       <Switch>
-          <Route exact path="/" component={MovieList} />
-          <Route path="/movies/new" component={NewMovie} />
-          <Route path="/movies/:id/edit" component={EditMovie} />
-          <Route path="/movies/:id" component={MovieDetails} />
-          <Route component={NotFound} />
-        </Switch>
+        <Route exact path="/" component={MovieList} />
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route path="/movies/:id" component={MovieDetails} />
+        <Route component={NotFound} />
+      </Switch>
     </Router>
   );
 }
