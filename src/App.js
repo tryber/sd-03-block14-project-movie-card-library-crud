50ef53bdda52566530215cfdb4e08a56e32b6591
henrique <hbaetaleite@gmail.com>
2020-05-12 23:40:48 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import MovieList from './pages/MovieList';
 
 import movies from './services/movieData';
 
-function App() {
-  return (
-    <Router>
-      <div>Movie Card Library Crud</div>
-      <MovieList movies={movies} />
-    </Router>
-  );
+class App extends Component() {
+  render() {
+    return (
+      <Router>
+        <div>Movie Card Library Crud</div>
+        <MovieList movies={movies} />
+      </Router>
+    );
+  }
 }
 
 export default App;
