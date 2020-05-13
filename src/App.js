4475218eb65fabd7577d4fa7d9e19391094a2791
Cardoso <lsc.cardosoo@gmail.com>
2020-05-13 11:14:37 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import * as Index from './pages/index';
 
 function App() {
   return (
-    <div>Movie Card Library Crud</div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Index.MovieList}/>
+        <Route path="/movies/:id" component={Index.MovieDetails}/>
+        <Route path="/movies/new" component={Index.NewMovie}/>
+        <Route path="/movies/:id/edit" component={Index.EditMovie}/>
+        <Route path="/" component={Index.NotFound}/>
+      </Switch>
+    </Router>
   );
 }
 
