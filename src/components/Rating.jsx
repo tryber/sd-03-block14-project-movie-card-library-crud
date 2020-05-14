7ff0a02e39fa247a87d787ec9fe3aeffbf777d32
renan.andreolle <renandreolle@hotmail.com>
2020-05-14 12:21:15 -0300

    proptypes6

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 class Rating extends React.Component {
   render() {
     const { rating } = this.props;
@@ -12,7 +13,7 @@ class Rating extends React.Component {
 }
 
 Rating.propTypes = {
-rating: PropTypes.number.isRequired,
+  rating: PropTypes.number.isRequired,
 };
 
 export default Rating;
