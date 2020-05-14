544c0f3145f1af918581eae472112a47ac51ce5f
rogerfaustino <roger_faustino@hotmail.com>
2020-05-14 10:43:22 -0300

    Implement elements

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
 import MovieForm from '../components/MovieForm';
 import * as movieAPI from '../services/movieAPI';
 
 class NewMovie extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      redirect: false,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(newMovie) {
+    movieAPI.createMovie(newMovie).then(this.setState({ redirect: true }));
   }
 
   render() {
+    const { redirect } = this.state;
+    const { history } = this.props;
+    if (redirect) history.push('/');
     return (
       <div data-testid="new-movie">
         <MovieForm onSubmit={this.handleSubmit} />
@@ -20,4 +27,11 @@ class NewMovie extends Component {
     );
   }
 }
+
 export default NewMovie;
+
+NewMovie.propTypes = {
+  history: PropTypes.objectOf(PropTypes
+    .oneOfType([PropTypes.string, PropTypes.number, PropTypes.bool]))
+    .isRequired,
+};
