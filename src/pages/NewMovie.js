4b0a3e0ec3fd0533c3945324cebc1da352b7ce2c
luis.campos <gucampos845@gmail.com>
2020-05-15 11:36:42 -0300

    Projeto Crud

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
 
+import PropTypes from 'prop-types';
 import MovieForm from '../components/MovieForm';
 import * as movieAPI from '../services/movieAPI';
 
 class NewMovie extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      shouldRedirect: false,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(newMovie) {
+    movieAPI.createMovie(newMovie).then(() => this.setState({ shouldRedirect: true }));
   }
 
   render() {
+    const { history } = this.props;
+    if (this.state.shouldRedirect) history.push('/');
     return (
       <div data-testid="new-movie">
         <MovieForm onSubmit={this.handleSubmit} />
@@ -20,4 +27,11 @@ class NewMovie extends Component {
     );
   }
 }
+
+NewMovie.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
 export default NewMovie;
