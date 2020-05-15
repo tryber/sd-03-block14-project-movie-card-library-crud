9fb7ca47076ab29724dc935b0131690da3338caa
Raphael Ribeiro dos Santos <raphal.r2021@gmail.com>
2020-05-15 11:31:17 -0300

    Project-movie-library-crud

--- src/services/movieAPI.js
+++ src/services/movieAPI.js
@@ -1,68 +1,54 @@
-import data from './movieData';
+const movies = [
+  {
+    id: 1,
+    title: 'Kingsglaive',
+    subtitle: 'Final Fantasy XV',
+    storyline: "King Regis, who oversees the land of Lucis, commands his army of soldiers to protect the kingdom from the Niflheim empire's plans to steal the sacred crystal.",
+    rating: 4.5,
+    imagePath: 'images/Kingsglaive_Final_Fantasy_XV.jpg',
+    bookmarked: true,
+    genre: 'action',
+  },
+  {
+    id: 2,
+    title: 'Final Fantasy',
+    subtitle: 'Spirits Within',
+    storyline: 'A scientist makes a last stand on Earth with the help of a ragtag team of soldiers against an invasion of alien phantoms.',
+    rating: 4.5,
+    imagePath: 'images/Final_Fantasy_Spirits_Within.jpg',
+    bookmarked: false,
+    genre: 'fantasy',
+  },
+  {
+    id: 3,
+    title: 'Ghost In The Shell',
+    subtitle: 'Ghost In The Shell',
+    storyline: 'A hacker known as the Puppet Master is hunted by a female cyborg cop and her partner. This film is a revised version of Ghost in the Shell (1995).',
+    rating: 5,
+    imagePath: 'images/Kingsglaive_Final_Fantasy_XV.jpg',
+    bookmarked: false,
+    genre: 'comedy',
+  },
+  {
+    id: 4,
+    title: 'Appleseed Alpha',
+    subtitle: 'Appleseed Alpha',
+    storyline: "A young female soldier Deunan and her cyborg partner Briareos survive through the post World War 3 apocalyptic New York in search of human's future hope, the legendary city of Olympus.",
+    rating: 3.8,
+    imagePath: 'images/Appleseed_Alpha.jpg',
+    bookmarked: true,
+    genre: 'action',
+  },
+  {
+    id: 5,
+    title: 'Resident Evil',
+    subtitle: 'Vendetta',
+    storyline: 'Chris Redfield enlists the help of Leon S. Kennedy and Rebecca Chambers to stop a death merchant, with a vengeance, from spreading a deadly virus in New York.',
+    rating: 4.2,
+    imagePath: 'images/Resident_Evil_Vendetta.jpg',
+    bookmarked: true,
+    genre: 'fantasy',
+  },
+];
 
-localStorage.setItem('movies', JSON.stringify(data));
-
-const readMovies = () => JSON.parse(localStorage.getItem('movies'));
-
-const saveMovies = (movies) => localStorage.setItem('movies', JSON.stringify(movies));
-
-export const getMovies = () => (
-  new Promise((resolve) => {
-    setTimeout(() => {
-      const movies = readMovies();
-      resolve(movies);
-    }, 2000);
-  })
-);
-
-export const getMovie = (movieId) => {
-  const movie = readMovies().find((mov) => mov.id === parseInt(movieId, 10));
-
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(movie);
-    }, 2000);
-  });
-};
-
-export const updateMovie = (updatedMovie) => {
-  const movies = readMovies().map((movie) => {
-    if (movie.id === parseInt(updatedMovie.id, 10)) {
-      return { ...movie, ...updatedMovie };
-    }
-    return movie;
-  });
-  saveMovies(movies);
-
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve('OK');
-    }, 1000);
-  });
-};
-
-export const createMovie = (movieData) => {
-  let movies = readMovies();
-  const nextId = movies[movies.length - 1].id + 1;
-  const newMovie = { ...movieData, id: nextId };
-  movies = [...movies, newMovie];
-  saveMovies(movies);
-
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve('OK');
-    }, 1000);
-  });
-};
-
-export const deleteMovie = (movieId) => {
-  let movies = readMovies();
-  movies = movies.filter((movie) => movie.id !== parseInt(movieId, 10));
-  saveMovies(movies);
-
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({ status: 'OK' });
-    }, 1000);
-  });
-};
+export default movies;
