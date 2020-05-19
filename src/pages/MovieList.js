b2e93ba20b29b08f5add681abaad6601bce0fcc8
lcdlima <leticia.duarte.lima@gmail.com>
2020-05-19 10:06:50 -0300

    Calling API, displaying waiting message and movies when API answers

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
 
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { movies: [] }
+  }
+
+  componentDidMount() {
+    movieAPI.getMovies().then((movies) => this.setState({ movies }));
+  }
+
   render() {
     const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
-
-    return (
-      <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
-      </div>
-    );
+    if (movies.length === 0) {
+      return <Loading />
+    } else {
+      return (
+        <div data-testid="movie-list">
+          {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+        </div>
+      )
+    }
   }
 }
 
