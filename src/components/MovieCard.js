9ee3b8531892fb040094bc394ebf03d6a6210b74
lcdlima <leticia.duarte.lima@gmail.com>
2020-05-19 10:20:12 -0300

    Creating MovieCard for each movie and adding Link to MovieDetails

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { title, storyline, imagePath, id } = movie;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <img src={imagePath} />
+        <p>{title}</p>
+        <p>{storyline}</p>
+        <Link 
+          to={{
+            pathname: `/movies/${id}`,
+            details: { movie }
+          }}
+          >
+            Ver Detalhes
+          </Link>
       </div>
     );
   }
