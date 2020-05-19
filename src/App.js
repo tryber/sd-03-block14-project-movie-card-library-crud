02d5b846df10a7e6457f0db1f2b8bcd62bab2eb7
lcdlima <leticia.duarte.lima@gmail.com>
2020-05-19 09:14:02 -0300

    Adding Routes in the App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Index from './pages/index';
+import MovieDetails from './pages/MovieDetails';
+import AddMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <div>Movie Card Library CRUD</div>
+      <Switch>
+          <Route exact path="/" component={Index} />
+          <Route exact path="/movies/:id" component={MovieDetails} />
+          <Route path="/movies/new" component={AddMovie} />
+          <Route path="/movies/:id/edit" component={EditMovie} />
+        </Switch>
     </Router>
   );
 }
