f6b08876cbece20281ac0ff879fa837ab1830cba
RobertoAsgon <60623542+RobertoAsgon@users.noreply.github.com>
2020-06-02 02:33:52 -0400

    Update MovieCard.js

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,11 +3,13 @@ import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
-    const { imagePath, title, storyline, id } = this.props.movie;
+    const { movie } = this.props;
+    const { id, title, subtitle, storyline, imagePath } = movie;
     return (
       <div data-testid="movie-card">
         <img src={imagePath} alt={title} />
         <h4>{title}</h4>
+        <h2>{subtitle}</h2>
         <p>{storyline}</p>
         <Link to={`movies/${id}`}>VER DETALHES</Link>
       </div>
