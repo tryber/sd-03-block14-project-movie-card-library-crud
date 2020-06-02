697487c5a904e8ac23ae3e3f37bdedd497765d04
RobertoAsgon <60623542+RobertoAsgon@users.noreply.github.com>
2020-06-02 02:56:40 -0400

    Update NewMovie.js

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -2,20 +2,18 @@ import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import MovieForm from '../components/MovieForm';
 import * as movieAPI from '../services/movieAPI';
+import PropTypes from 'prop-types';
 
 class NewMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      shouldRedirect: false,
-    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(newMovie) {
-    movieAPI.createMovie(newMovie).then(() => this.setState({
-      shouldRedirect: true,
-    }));
+    const { history } = this.props;
+    movieAPI.createMovie(newMovie);
+    history.push('/');
   }
 
   render() {
@@ -29,3 +27,10 @@ class NewMovie extends Component {
   }
 }
 export default NewMovie;
+
+NewMovie.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
