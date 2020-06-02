cced03ea626a18873101e66a873f5b58f2ab4eaa
RobertoAsgon <60623542+RobertoAsgon@users.noreply.github.com>
2020-06-02 02:56:21 -0400

    Update MovieDetails.js

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
+import PropTypes from 'prop-types';
 
 class MovieDetails extends Component {
   constructor(props) {
@@ -45,3 +46,11 @@ class MovieDetails extends Component {
 }
 
 export default MovieDetails;
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
