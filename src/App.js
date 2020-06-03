f360e28c3cda68416645981140a908d82b373831
Luma Arruda <luma.oarruda@gmail.com>
2020-06-03 18:51:45 -0300

    requisito 1 - setando BrowseRouter e organuzando caminhos das rotas - inicialmente comentado para rodar a aplicaçao apenas apos o desenvolvimento dos componentes correnpondentes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+// import * as index from './pages/index';
 
 function App() {
   return (
-    <div>Movie Card Library Crud</div>
+    <div>
+      <h1>Movie Card Library Crud</h1>
+      <BrowserRouter>
+        <Switch>
+          {/* <Route path="/movies/:id" component={index.MovieDetails} />
+          <Route path="/movies/:id/edit" component={index.EditMovie} />
+          <Route path="/movies/new" component={index.NewMovie} />
+          <Route exact path="/" component={index.MovieList} />
+          <Route component={index.NotFound} /> */}
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
