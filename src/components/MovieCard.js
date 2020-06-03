95e391631ac83073c4eb5524ac5fe2b0293ca7c6
Luma Arruda <luma.oarruda@gmail.com>
2020-06-03 19:31:13 -0300

    requisito 3 - movieCard - definindo as informaçoes do card na pagina inicial e o link para detalhes

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath } = movie;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <img src={imagePath} alt={`${title} movie cover`} />
+        <h3>{title}</h3>
+        <h4>{subtitle}</h4>
+        <p>{storyline}</p>
+        <Link to={`/movies/${id}`}>Ver Detalhes</Link>
       </div>
     );
   }
